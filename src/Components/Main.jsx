1a9c2a1a113b5ab38a6c7b7d7a2084a75c9cab48
hetpatel <hetfaldu19@gmail.com>
2025-04-06 15:27:56 -0400

    Implement add ingredient functionality

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,14 +1,31 @@
+import React from 'react';
+
 export default function Main() {
+    const [ingredientList, setIngredientList] = React.useState([]);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const newIngredient = formData.get("ingredient");
+        setIngredientList(prevList => [...prevList, newIngredient]);
+    }
+
     return (
         <main>
-            <forms className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. Milk"
                     aria-label="Add Ingredient" 
+                    name="ingredient"
                 />
                 <button>Add Ingredient</button>
-            </forms>
+            </form>
+            <ul className="ingredient-list">
+                {ingredientList.map((ingredient, index) => (
+                    <li key={index}>{ingredient}</li>
+                ))}
+            </ul>
         </main>
     )
 }
